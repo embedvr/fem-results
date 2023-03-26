d069ae6711684c71f6fa5fa4b2e350998cc1a157
embed <brydon@helium.email>
2023-03-26 03:29:56 -0400

    [~] fix other bugs and close the right tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,26 +12,26 @@
 <div
 	class="flex h-full flex-col rounded-[2rem] sm:h-fit sm:flex-row sm:bg-white sm:drop-shadow-xl"
 >
-	<main
+	<header
 		class="flex h-[22rem] flex-col items-center justify-between rounded-b-[2rem] bg-gradient-to-b from-indigo-600 to-indigo-700 p-7 sm:h-[27rem] sm:w-[20rem] sm:rounded-[2rem]"
 	>
 		<h1 class="text-md font-semibold text-gray-300 opacity-75">Your Result</h1>
 		<div
 			class="flex h-36 w-36 flex-col items-center justify-center rounded-full bg-gradient-to-b from-indigo-800 to-transparent sm:h-40 sm:w-40"
 		>
-			<h2 class="text-5xl font-bold text-white">76</h3>
+			<h2 class="text-5xl font-bold text-white">76</h2>
 			<span class="font-medium text-gray-300 opacity-75">of 100</span>
 		</div>
 		<div class="flex flex-col items-center justify-center gap-1 text-center">
-			<h3 class="text-2xl font-bold text-white">Great</h2>
+			<h3 class="text-2xl font-bold text-white">Great</h3>
 			<span class="font-medium text-gray-300 opacity-75"
 				>You scored higher than 69% of the people who have taken these tests.</span
 			>
 		</div>
-	</main>
-	<div class="flex w-full flex-col sm:w-[20rem]">
+	</header>
+	<main class="flex w-full flex-col sm:w-[20rem]">
 		<div class="px-7 py-5">
-			<h4 class="text-lg font-medium">Summary</h4>
+			<h4 class="text-lg font-medium" role="heading">Summary</h4>
 			<div class="mt-4 flex flex-col justify-around gap-3">
 				<div
 					class="flex w-full flex-row items-center justify-between rounded-xl bg-red-300 bg-opacity-10 p-4"
@@ -40,11 +40,11 @@
 						class="flex flex-row items-center gap-3 font-medium text-red-400"
 					>
 						<MaterialSymbolsBoltOutline class="h-5 w-5" />
-						<span>Reaction</span>
+						<span role="text">Reaction</span>
 					</div>
 					<div class="font-medium">
-						<span class="text-black">80</span>
-						<span class="text-gray-400"> / 100</span>
+						<span class="text-black" role="number">80</span>
+						<span class="text-gray-400" role="number"> / 100</span>
 					</div>
 				</div>
 				<div
@@ -54,11 +54,11 @@
 						class="flex flex-row items-center gap-3 font-medium text-yellow-400"
 					>
 						<IconoirBrain class="h-5 w-5" />
-						<span>Memory</span>
+						<span role="text">Memory</span>
 					</div>
 					<div class="font-medium">
-						<span class="text-black">92</span>
-						<span class="text-gray-400"> / 100</span>
+						<span class="text-black" role="number">92</span>
+						<span class="text-gray-400" role="number"> / 100</span>
 					</div>
 				</div>
 				<div
@@ -68,11 +68,11 @@
 						class="flex flex-row items-center gap-3 font-medium text-green-400"
 					>
 						<IonChatbubbleEllipsesOutline class="h-5 w-5" />
-						<span>Verbal</span>
+						<span role="text">Verbal</span>
 					</div>
 					<div class="font-medium">
-						<span class="text-black">61</span>
-						<span class="text-gray-400"> / 100</span>
+						<span class="text-black" role="number">61</span>
+						<span class="text-gray-400" role="number"> / 100</span>
 					</div>
 				</div>
 				<div
@@ -82,11 +82,11 @@
 						class="flex flex-row items-center gap-3 font-medium text-blue-400"
 					>
 						<MdiEyeOutline class="h-5 w-5" />
-						<span>Visual</span>
+						<span role="text">Visual</span>
 					</div>
 					<div class="font-medium">
-						<span class="text-black">72</span>
-						<span class="text-gray-400"> / 100</span>
+						<span class="text-black" role="number">72</span>
+						<span class="text-gray-400" role="number"> / 100</span>
 					</div>
 				</div>
 			</div>
@@ -98,5 +98,5 @@
 				Continue
 			</button>
 		</div>
-	</div>
+	</main>
 </div>
